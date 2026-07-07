202e1c491ee37a67bb4d33f100a6e2b0b774e379
kernelbuild-KutuMoba <kernelbuild92@gmail.com>
2026-07-08 04:49:39 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ echo "Cloning dependencies"
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable  clang
 git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
 git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
-git clone --depth=1 https://github.com/sohamxda7/AnyKernel3 AnyKernel
+git clone --depth=1 https://github.com/kernelbuild-KutuMoba/AnyKernel3 AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
